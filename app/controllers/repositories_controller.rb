0aee6d4abb0f21833a5adcaa14f32627465384ba
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-22 11:02:36 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,12 @@
 class RepositoriesController < ApplicationController
 
   def index
-    binding.pry
     resp = Faraday.get "https://api.github.com/user" do |req|
       req.headers['Authorization'] = "token " + session[:token]
     end
-
+    binding.pry
+    body = JSON.parse(resp.body)
+    @username = body["login"]
   end
 
 end
