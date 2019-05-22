dfb46c1dac09ee78193b172990c406f692c54d2d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-22 10:52:45 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,6 @@ class SessionsController < ApplicationController
       req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
       req.headers['Accept'] = 'application/json'
     end
-    binding.pry
-
     body = JSON.parse(response.body)
     session[:token] = body["access_token"]
     redirect_to root_path
